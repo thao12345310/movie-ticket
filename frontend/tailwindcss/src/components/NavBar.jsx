08025118f607f69b2bb7f5abbf1b709d1d4cf955
Thao Duong <duongphuongthao08102004@gmail.com>
2025-02-18 22:41:28 +0700

    fix hover

--- frontend/tailwindcss/src/components/NavBar.jsx
+++ frontend/tailwindcss/src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Disclosure,
   DisclosureButton,
@@ -9,7 +10,7 @@ import {
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
+const initialNavigation = [
   { name: "Đang chiếu", href: "dang-chieu", current: true },
   { name: "Đồ ăn/Combo", href: "do-an-combo", current: false },
   { name: "Khuyến mãi", href: "khuyen-mai", current: false },
@@ -21,6 +22,16 @@ function classNames(...classes) {
 }
 
 export default function NavBar() {
+  const [navigation, setNavigation] = useState(initialNavigation);
+
+  const handleMenuClick = (name) => {
+    setNavigation((prevNavigation) =>
+      prevNavigation.map((item) => ({
+        ...item,
+        current: item.name === name, // Đặt mục được click thành current
+      }))
+    );
+  };
   return (
     <Disclosure as="nav" className="bg-gray-800">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -40,7 +51,9 @@ export default function NavBar() {
               />
             </DisclosureButton>
           </div>
+
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            {/* Logo */}
             <div className="flex shrink-0 items-center">
               <img
                 alt="Your Company"
@@ -48,13 +61,16 @@ export default function NavBar() {
                 className="h-8 w-auto"
               />
             </div>
+
+            {/* Menu Items */}
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a
+                  <button
                     key={item.name}
                     href={item.href}
                     aria-current={item.current ? "page" : undefined}
+                    onClick={() => handleMenuClick(item.name)}
                     className={classNames(
                       item.current
                         ? "bg-gray-900 text-white"
@@ -63,11 +79,13 @@ export default function NavBar() {
                     )}
                   >
                     {item.name}
-                  </a>
+                  </button>
                 ))}
               </div>
             </div>
           </div>
+
+          {/* Bell */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
@@ -125,6 +143,7 @@ export default function NavBar() {
         </div>
       </div>
 
+      {/* Menu trên Mobile */}
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
           {navigation.map((item) => (
